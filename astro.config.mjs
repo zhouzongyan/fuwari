b49085a84bca35aff498b5b6aee2297828ed3a5f
allen.zhou <allen@chn.cc>
2026-01-15 09:57:03 +0800

    修复文章搜索链接不对的问题

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,7 +34,7 @@ export default defineConfig({
 	image: {
 		service: passthroughImageService(),
 	},
-	site: "https://blog.acofork.com",
+	site: "https://blog.wemang.com",
 	base: "/",
 	trailingSlash: "always",
 	output: "static",
