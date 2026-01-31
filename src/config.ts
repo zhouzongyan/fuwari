43ce1bbc20bf1bc052adb953e3d86797456bda45
allen.zhou <allen@chn.cc>
2026-01-31 17:25:57 +0800

    优化页面跳转链接提示

--- src/config.ts
+++ src/config.ts
@@ -124,7 +124,7 @@ export const profileConfig: ProfileConfig = {
 export const licenseConfig: LicenseConfig = {
 	enable: true,
 	name: "CC BY-NC-SA 4.0",
-	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh-hans",
 };
 
 export const imageFallbackConfig: ImageFallbackConfig = {
